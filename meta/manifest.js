78f5923b8726a262e9425350c430432ce177c2ac
JP Archer <jp.archer@inviqa.com>
2019-01-25 11:46:19 +0000

    Added correct font src

--- meta/manifest.js
+++ meta/manifest.js
@@ -166,7 +166,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49703" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64785" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
